8f91a91dfc7f711c50f48e8f8bde4a2223c93c06
Balam <lufemega@hotmail.com>
2023-11-20 17:20:12 -0600

    DbContext - Inyeccion de Dependencias

---
+++ MagicVilla_API/Modelos/Dto/VillaDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicVilla_API.Modelos.Dto
+{
+    public class VillaDto
+    {
+        [Key] 
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Detalle { get; set; }
+        [Required]
+        public double Tarifa { get; set; }
+        public int Ocupantes { get; set; }
+        public double MetrosCuadrados { get; set; }
+        public string Amenidad { get; set; }
+        public string ImagenUrl { get; set; }
+    }
+}
